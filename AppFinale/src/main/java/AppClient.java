ca29dcdf1d15426ab67244021f9ed5c035517fb1
Sanchez <jocelin.sanchez@etu.umontpellier.fr>
2022-10-23 19:18:08 +0200

    Changements dans AppClient afin de permettre à l'utilisateur de choisir ce qu'il souhaitre faire (voter/consulter/quitter)

--- AppFinale/src/main/java/AppClient.java
+++ AppFinale/src/main/java/AppClient.java
@@ -1,11 +1,40 @@
+import java.io.IOException;
+import java.util.Scanner;
+
 public class AppClient {
 
     public static void main(String[] args) throws ClassNotFoundException {
         System.out.println("Création du client...");
-        Client client = new Client();
-        System.out.println("Client connecté");
+        Client client;
+        try{
+            client = new Client();
+            System.out.println("Client connecté");
 
-        System.out.println(client.consulterVoteEnCour());
-        client.voter();
+
+            String input;
+            Scanner sc = new Scanner(System.in);
+
+            do {
+                System.out.println("Choisir une action : \n-consulter le vote en cours [c]\n-voter [v]\n-quitter [q]");
+                input = sc.nextLine();
+
+                switch (input){
+                    case "v" :
+                        client.voter();
+                        break;
+                    case "c" :
+                        System.out.println(client.consulterVoteEnCour());
+                        break;
+                    case "q" :
+                        break;
+                    default:
+                        System.out.println("Commande non-reconnue");
+                }
+
+            }while(!input.equals("v") && !input.equals("q"));
+
+        }catch (IOException e){
+            System.out.println("La connexion au serveur n'a pas pu être établie");
+        }
     }
 }
