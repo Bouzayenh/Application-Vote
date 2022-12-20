47410b858ce5c9a0a7caa05ead5c1528b42d9825
Jocelin Sanchez <jocelin.sanchez@etu.umontpellier.fr>
2022-12-20 15:22:15 +0100

    Ajout de quelques méthodes dans Mail, afin que chaque type de mail utilise une méthode différente, avec des arguments différents, Cela ne marche pas, car les mails ne marchent pas.

--- JavaFX/src/main/java/dataobject/Mail.java
+++ JavaFX/src/main/java/dataobject/Mail.java
@@ -58,6 +58,81 @@ public class Mail {
         return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
     }
 
+    /**
+     * Envoie un mail notifiant la fin d'un vote.
+     * @param email L'adresse email à laquelle le courrier éléctronique doit être envoyé.
+     * @param vote Le vote qui vient de se terminer.
+     */
+    public void envoyerMailFinVote(String email, Vote vote){
+
+        String messageBody =
+                "Bonjour,\n" +
+                "Un vote auquel vous avez participé vient de se terminer.\n" +
+                "L'intitulé du référundum était : « " + vote.getIntitule() + " »\n" +
+                "Vous aviez le choix entre les options : « " + vote.getOption1() + " » et «" + vote.getOption2() + " »\n";
+
+            if (vote.getResultat() < 0.5)
+
+                messageBody += "L'option « " + vote.getOption1() + " » gagne avec "
+                        + ((double) (int) ((1 - vote.getResultat()) * 10000)) / 100 + "% des voix";
+
+            else if (vote.getResultat() > 0.5)
+
+                messageBody += "L'option « " + vote.getOption2() + " » gagne avec "
+                        + ((double) (int) (vote.getResultat() * 10000)) / 100 + "% des voix";
+
+            else
+
+                messageBody += "Les options « " + vote.getOption1() +
+                        " » et « " + vote.getOption2() + " » sont à égalité";
+
+        envoyerMail2(email, "E-Scrutin : Fin d'un vote auquel vous avez participé.", messageBody);
+    }
+
+    /**
+     * Envoie un mail notifiant la participation à un vote.
+     * @param email L'adresse email à laquelle le courrier éléctronique doit être envoyé.
+     * @param vote Le vote pour lequel le bulletin a été dépose.
+     */
+    public void envoyerMailDepotBulletin(String email, Vote vote){
+        envoyerMail2(
+                email,
+                "E-Scrutin : Participation à un vote.",
+                "Bonjour,\n" +
+                        "Nous confirmons le dépôt de votre bulletin pour le vote suivant : \n" +
+                        vote.getIntitule() + "\n" +
+                        "Si la participation à ce référundum ne vient pas de vous, merci de vous rendre auprès de l'administrateur système."
+        );
+    }
+
+
+    /**
+     * Envoie un mail notifiant la création d'un compte dans le système.
+     * @param email L'adresse email à laquelle le courrier éléctronique doit être envoyé.
+     * @param utilisateur L'{@link Utilisateur} qui vient d'être créé (mot de passe en clair).
+     */
+    public void envoyerMailCreationCompte(String email, Utilisateur utilisateur){
+        envoyerMail2(
+                email,
+                "E-scrutin : Création d'un compte.",
+                "Bonjour,\n" +
+                        "Un compte vous a été attribué au sein de l'entreprise, voici vos identifiants de connexion : " +
+                        "Nom d'utilisateur : " + utilisateur.getLogin() + "\n" +
+                        "Mot de passe : " + utilisateur.getMotDePasse() + "\n\n" +
+                        "Merci de changer de mot de passe le plus tôt possible sur un des postes de l'entreprise."
+        );
+    }
+
+    /**
+     * Envoie un mail quelconque.
+     * @param email L'adresse email à laquelle le courrier éléctronique doit être envoyé.
+     * @param messageSubject Le champs <strong>sujet</strong> du courrier éléctronique envoyé.
+     * @param messageBody Le corps du mail envoyé.
+     */
+    private void envoyerMail2(String email, String messageSubject, String messageBody){
+
+    }
+
     public void envoyerMail(String subject,String toEmailAddress,Vote vote) throws GeneralSecurityException, IOException, MessagingException {
         String messageSubject = null;
         String messageBody= null;
