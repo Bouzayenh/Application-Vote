0b6cd6a02ae7e390db580095484c9710c700851f
Vredeza <53372645+Vredeza@users.noreply.github.com>
2023-03-14 00:18:08 +0100

    Commande secrète

--- JavaFX/src/main/java/app/AppServeur.java
+++ JavaFX/src/main/java/app/AppServeur.java
@@ -4,229 +4,204 @@ import controller.Serveur;
 import dataobject.Utilisateur;
 import dataobject.Vote;
 import dataobject.exception.FeedbackException;
+import dataobject.exception.MauvaisMotDePasseServeurException;
 
 import java.io.IOException;
 import java.sql.SQLException;
-import java.time.DateTimeException;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
+import java.time.*;
 import java.util.Scanner;
 import java.util.Set;
 
 public class AppServeur {
 
+    static Serveur serveur;
+
     public static void main(String[] args) {
+
         try {
             // initialisation
             System.out.println("Ouverture du serveur...");
-            Serveur serveur = Serveur.getInstance();
+            serveur = Serveur.getInstance();
             System.out.println("Serveur ouvert. Lancement...");
             serveur.run();
             System.out.println("Serveur lancé. Connexions possibles");
 
-            Scanner sc = new Scanner(System.in);
-            String intitule, option1, option2, idVote, login, email;
-
-            // interface de l'administrateur
             while (true) {
-                System.out.print("\nChoisissez l'une des options suivantes :\n"
-                        + " [1] Consulter la liste des votes\n"
-                        + " [2] Créer un vote\n"
-                        + " [3] Terminer un vote\n"
-                        + " [4] Consulter les résultats d'un vote\n"
-                        + " [5] Consulter la liste des utilisateurs\n"
-                        + " [6] Créer un utilisateur\n"
-                        + " [7] Supprimer un utilisateur\n"
-                        + " [8] Modifier un utilisateur\n"
-                        + " [x] Arrêter le serveur\n"
-                        + "> ");
-                String input = sc.nextLine().toLowerCase();
+                while (serveur.estVerouille()){
+                    try {
+                        serveur.deverouiller(demanderString("Entrez le mot de passe afin de dévérouiller le serveur"));
+                        System.out.println("Serveur dévérouillé.");
 
-                try {
-                    switch (input) {
-                        // TODO on pourra externaliser chaque cas comme méthode pour que ce soit plus clair
-
-                        case "1":
-                            Set<Vote> votes = serveur.consulterVotes();
-                            for (Vote vote : votes) {
-                                System.out.print("[" + vote.getIdentifiant() + "] " + vote.getIntitule()
-                                        + " (" + vote.getOption1() + " ou " + vote.getOption2() + ")");
-                                if (vote.estFini())
-                                    System.out.println(" - Terminé");
-                                else
-                                    System.out.println();
-                            }
-                            break;
-
-                        case "2":
-                            System.out.print("Entrez [q] à tout moment pour annuler la création du vote :\n"
-                                    + "Intitulé du vote :\n"
-                                    + "> ");
-                            intitule = sc.nextLine();
-                            if (intitule.equals("q"))
-                                break;
-
-                            System.out.print("Première option :\n"
-                                    + "> ");
-                            option1 = sc.nextLine();
-                            if (option1.equals("q"))
-                                break;
-
-                            System.out.print("Deuxième option :\n"
-                                    + "> ");
-                            option2 = sc.nextLine();
-                            if (option2.equals("q"))
-                                break;
-
-                            int jourDeFin, moisDeFin, anneeDeFin, heureDeFin;
-                            System.out.print("Jour de fin :\n"
-                                    + "> ");
-                            try {
-                                jourDeFin = Integer.parseInt(sc.nextLine());
-                            }catch (NumberFormatException e) {break;}
-
-                            System.out.print("Mois de fin :\n"
-                                    + "> ");
-                            try {
-                                moisDeFin = Integer.parseInt(sc.nextLine());
-                            }catch (NumberFormatException e) {break;}
-
-                            System.out.print("Annee de fin :\n"
-                                    + "> ");
-                            try {
-                                anneeDeFin = Integer.parseInt(sc.nextLine());
-                            }catch (NumberFormatException e) {break;}
-
-                            System.out.print("Heure de fin :\n"
-                                    + "> ");
-                            try {
-                                heureDeFin = Integer.parseInt(sc.nextLine());
-                            }catch (NumberFormatException e) {break;}
-
-                            LocalDateTime dateFin = LocalDateTime.of(LocalDate.of(
-                                    anneeDeFin, moisDeFin, jourDeFin),
-                                    LocalTime.of(heureDeFin, 0)
-                            );
-
-                            serveur.creerVote(intitule, option1, option2, dateFin);
-                            System.out.println("Vote créé");
-                            break;
-
-                        case "3":
-                            System.out.print("Entrez [q] à tout moment pour annuler la terminaison du vote :\n"
-                                    + "Identifiant du vote :\n"
-                                    + "> ");
-                            idVote = sc.nextLine();
-                            if (idVote.equals("q"))
-                                break;
-
-                            serveur.terminerVote(Integer.parseInt(idVote));
-                            System.out.println("Vote terminé");
-                            break;
-
-                        case "4":
-                            System.out.print("Entrez [q] à tout moment pour annuler la consultation des résultats du vote :\n"
-                                    + "Identifiant du vote :\n"
-                                    + "> ");
-                            idVote = sc.nextLine();
-                            if (idVote.equals("q"))
-                                break;
-
-                            Vote vote = serveur.consulterResultats(Integer.parseInt(idVote));
-                            System.out.println(vote.getIntitule());
-                            if (vote.getNbBulletins() == 0)
-                                System.out.println("Personne n'a participé à ce vote");
-                            else {
-                                if (vote.getResultat() < 0.5)
-                                    System.out.println("L'option « " + vote.getOption1() + " » gagne avec "
-                                            + ((double) (int) ((1 - vote.getResultat()) * 10000)) / 100 + "% des voix");
-                                else if (vote.getResultat() > 0.5)
-                                    System.out.println("L'option « " + vote.getOption2() + " » gagne avec "
-                                            + ((double) (int) (vote.getResultat() * 10000)) / 100 + "% des voix");
-                                else
-                                    System.out.println("Les options « " + vote.getOption1() +
-                                            " » et « " + vote.getOption2() + " » sont à égalité");
-                            }
-                            break;
-
-                        case "5":
-                            Set<Utilisateur> utilisateurs = serveur.consulterUtilisateurs();
-                            for (Utilisateur utilisateur : utilisateurs)
-                                System.out.println(utilisateur.getLogin() + " (" + utilisateur.getEmail() + ")");
-                            break;
-
-                        case "6":
-                            System.out.print("Entrez [q] à tout moment pour annuler la création de l'utilisateur :\n"
-                                    + "Login :\n"
-                                    + "> ");
-                            login = sc.nextLine();
-                            if (login.equals("q"))
-                                break;
-
-                            System.out.print("Adresse email :\n"
-                                    + "> ");
-                            email = sc.nextLine();
-                            if (email.equals("q"))
-                                break;
-
-                            serveur.creerUtilisateur(login, email, 10);
-                            System.out.println("Utilisateur créé");
-                            break;
-
-                        case "7":
-                            System.out.print("Entrez [q] à tout moment pour annuler la suppression de l'utilisateur :\n"
-                                    + "Login :\n"
-                                    + "> ");
-                            login = sc.nextLine();
-                            if (login.equals("q"))
-                                break;
-
-                            serveur.supprimerUtilisateur(login);
-                            System.out.println("Utilisateur supprimé");
-                            break;
-
-                        case "8":
-                            System.out.print("Entrez [q] à tout moment pour annuler la modification de l'utilisateur :\n"
-                                    + "Login :\n"
-                                    + "> ");
-                            login = sc.nextLine();
-                            if (login.equals("q"))
-                                break;
-
-                            System.out.print("Nouvelle adresse email (rien pour ne pas changer)\n" +
-                                    "> ");
-                            email = sc.nextLine();
-                            if (email.equals("q"))
-                                break;
-
-                            serveur.modifierUtilisateurEmail(login, email);
-                            System.out.println("Utilisateur modifié");
-                            break;
-
-                        case "x":
-                            System.exit(0); // TODO Extrêmement irresponsable
-
-                        default:
-                            System.out.println("Commande non reconnue");
+                    }catch (MauvaisMotDePasseServeurException e){
+                        System.out.println(e.getMessage());
                     }
-                } catch (FeedbackException e) {
-                    System.out.println("Erreur : " + e.getMessage());
-                } catch (NumberFormatException e) {
-                    System.out.println("Erreur : Pas un nombre");
-                }catch (DateTimeException e) {
-                    System.out.println("Erreur : Date invalide");
-                } catch (IOException | ClassNotFoundException | NullPointerException e) {
-                    if (!serveur.estConnecteScrutateur())
-                        System.out.println("Erreur : Impossible de se connecter au scrutateur");
-                    else
-                        throw e;
+                }
+
+                while (!serveur.estVerouille()){
+
+                    try {
+
+                        int choix = demanderInt(
+                                "\nChoisissez l'une des options suivantes :\n"
+                                        + " [1] Consulter la liste des votes\n"
+                                        + " [2] Créer un vote\n"
+                                        + " [3] Consulter les résultats d'un vote\n"
+                                        + " [4] Consulter la liste des utilisateurs\n"
+                                        + " [5] Créer un utilisateur\n"
+                                        + " [6] Modifier un utilisateur\n"
+                                        + " [7] Supprimer un utilisateur\n"
+                                        + " [8] Changer le mot de passe du serveur\n"
+                                        + " [9] Verrouiller le serveur\n"
+                                        + " [0] Arrêter le serveur"
+                        );
+
+                        switch (choix) {
+                            case 1 -> afficherVotes();
+                            case 2 -> creerVote();
+                            case 3 -> consulterResultat();
+                            case 4 -> consulterListeUtilisateur();
+                            case 5 -> creerUtilisateur();
+                            case 6 -> modifierUtilisateur();
+                            case 7 -> supprimerUtilisateur();
+                            case 8 -> changerMotDePasse();
+                            case 9 -> verrouillerServeur();
+                            case 0 -> arreterServeur();
+                            case 1111 -> terminerVote();
+                            case -1 -> System.out.println("Commande non-reconnue.");
+                        }
+                    } catch (FeedbackException e) {
+                        System.out.println("Erreur : " + e.getMessage());
+                    } catch (ClassNotFoundException | IOException | NullPointerException e){
+                        System.out.println("Erreur : Impossible de se connecter au scrutateur.");
+                    } catch (DateTimeException e){
+                        System.out.println("Erreur : Date invalide.");
+                    }
                 }
             }
-        } catch (IOException | ClassNotFoundException | SQLException e) {
+
+        } catch (IOException | SQLException e) {
+            e.printStackTrace();
             System.out.println("Erreur critique : Arrêt du serveur");
         }
     }
+
+    public static String demanderString (String intitule){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(intitule);
+        System.out.print(">");
+
+        return scanner.nextLine();
+    }
+
+    public static int demanderInt (String intitule){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(intitule);
+        System.out.print(">");
+
+        try {
+            return Integer.parseInt(scanner.nextLine());
+        }catch (NumberFormatException e){
+            return -1;
+        }
+    }
+
+    public static void afficherVotes() throws FeedbackException {
+        Set<Vote> votes = serveur.consulterVotes();
+        for (Vote vote : votes) {
+            System.out.print("[" + vote.getIdentifiant() + "] " + vote.getIntitule()
+                    + " (" + vote.getOption1() + " ou " + vote.getOption2() + ")");
+            if (vote.estFini())
+                System.out.println(" - Terminé");
+            else
+                System.out.println();
+        }
+    }
+
+    public static void creerVote() throws FeedbackException, IOException, ClassNotFoundException, DateTimeException {
+        serveur.creerVote(
+                demanderString("Intitulé du vote :"),
+                demanderString("Option 1 :"),
+                demanderString("Option 2 :"),
+                LocalDateTime.of(
+                        LocalDate.of(
+                                demanderInt("Annee de fin :"),
+                                demanderInt("Mois de fin :"),
+                                demanderInt("Jour de fin :")
+                        ),
+                        LocalTime.of(demanderInt("Heure de fin :"), 0)
+                )
+        );
+    }
+
+    public static void consulterResultat() throws FeedbackException {
+
+        Vote vote = serveur.consulterResultats(demanderInt("Identifiant du vote : "));
+        System.out.println(vote.getIntitule());
+        if (vote.getNbBulletins() == 0)
+            System.out.println("Personne n'a participé à ce vote");
+        else {
+            if (vote.getResultat() < 0.5)
+                System.out.println("L'option « " + vote.getOption1() + " » gagne avec "
+                        + ((double) (int) ((1 - vote.getResultat()) * 10000)) / 100 + "% des voix");
+            else if (vote.getResultat() > 0.5)
+                System.out.println("L'option « " + vote.getOption2() + " » gagne avec "
+                        + ((double) (int) (vote.getResultat() * 10000)) / 100 + "% des voix");
+            else
+                System.out.println("Les options « " + vote.getOption1() +
+                        " » et « " + vote.getOption2() + " » sont à égalité");
+        }
+    }
+
+    public static void consulterListeUtilisateur() throws SQLException, FeedbackException {
+        Set<Utilisateur> utilisateurs = serveur.consulterUtilisateurs();
+        for (Utilisateur utilisateur : utilisateurs)
+            System.out.println(utilisateur.getLogin() + " (" + utilisateur.getEmail() + ")");
+    }
+
+    public static void creerUtilisateur() throws FeedbackException {
+        serveur.creerUtilisateur(
+                demanderString("Login : "),
+                demanderString("Adresse E-Mail : "),
+                10
+        );
+    }
+
+    public static void modifierUtilisateur() throws SQLException, FeedbackException {
+
+        serveur.modifierUtilisateurEmail(
+                demanderString("Login : "),
+                demanderString("Nouvelle adresse E-Mail : ")
+        );
+    }
+
+    public static void supprimerUtilisateur() throws SQLException, FeedbackException {
+
+        serveur.supprimerUtilisateur(demanderString("Login : "));
+        System.out.println("Utilisateur supprimé");
+    }
+
+    public static void verrouillerServeur(){
+        System.out.println("Verrouillage du serveur.");
+        serveur.verouiller();
+    }
+
+    public static void arreterServeur(){
+        System.out.println("Arrêt du serveyr");
+        System.exit(0); //extrêmement irresponsable
+    }
+
+    public static void changerMotDePasse() throws FeedbackException {
+        serveur.changerMotDePasseServeur(
+                demanderString("Ancien mot de passe : "),
+                demanderString("Nouveau mot de passe : ")
+        );
+
+        System.out.println("Le mot de passe du serveur a bien été changé.");
+    }
+
+    public static void terminerVote() throws FeedbackException, IOException, ClassNotFoundException {
+        serveur.terminerVote(demanderInt("Identifiant du vote : "));
+    }
 }
-
-
